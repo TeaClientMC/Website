4343e80f1d9c704cd5576df09a40ba10a5907b6c
eveeify <eveeg1971@gmail.com>
2023-08-02 13:00:00 +1000

    added ssl.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const https = require('https')
+const fs = require('fs')
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
@@ -14,9 +16,22 @@ app.get("/discord", function (req, res) {
   res.redirect("https://www.discord.gg/teaclient")
 });
 
+app.get("/docs", function (req, res) {
+  res.redirect("https://www.docs.teaclient.net");
+});
+
+
 app.use(function (req, res) {
   res.status(404).sendFile(path.join(__dirname + "/page/404.html"));
 });
 
 
-app.listen(3000)
+const SSLserver = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "ssl", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "ssl", "cert.pem")),
+  },
+  app
+);
+
+  SSLserver.listen(3000, () => console.log("Online at https:localhost:3000"));

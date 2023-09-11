03e37461ac66d79f3473b99c226a63097199e844
eveeifyeve <eveeg1971@gmail.com>
2023-09-11 13:27:18 +1000

    added a bunch of updates

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 Bun.serve({
   port: 3000,
+  hostname: '',
   fetch(req) {
 
-    // Home page
     const url = new URL(req.url);
+    let response;
+    
     if (url.pathname === "/") {
-      return new Response(Bun.file("./Src/HTML/index.html"), {
-        headers: {
-          "Content-Type": "text/html, charset=UTF-8",
-        },
-      });
+        console.log('Starting Home page');
+        response = new Response(Bun.file("./Src/HTML/index.html"), {
+            headers: {
+                "Content-Type": "text/html, charset=UTF-8",
+            },
+        });
+    } else if (url.pathname === "/download") {
+        console.log('Starting Download page');
+        response = new Response(Bun.file("./src/HTML/download.html"), {
+            headers: {
+                "Content-Type": "text/html, charset=UTF-8",
+            },
+        });
+    } else if (url.pathname === "/discord") {
+        console.log('Discord Redirecting');
+        response = Response.redirect("https://discord.gg/teaclient");
     } else if (url.pathname.startsWith("/CSS/")) {
-      return new Response(Bun.file("./Src" + url.pathname), {
-        headers: {
-          "Content-Type": "text/css",
-        },
-      });
-    } else {
-      return new Response("Not Found", { status: 404 });
+        response = new Response(Bun.file("./Src" + url.pathname), {
+            headers: {
+                "Content-Type": "text/css",
+            },
+        });
     }
+    
+    return response;
+    
+
+
+
+
 
   },
 });

b8ce99dd34b08348bc3be65a2313e970ff849906
eveeify <eveeg1971@gmail.com>
2023-09-13 03:28:52 +1000

    got it working.

--- index.js
+++ index.js
@@ -1,62 +1,33 @@
 Bun.serve({
   port: 3000,
-  hostname: '',
+  hostname: "",
   fetch(req) {
-
     const url = new URL(req.url);
     let response;
-    
+
     if (url.pathname === "/") {
-        console.log('Starting Home page');
-        response = new Response(Bun.file("./Src/HTML/index.html"), {
-            headers: {
-                "Content-Type": "text/html, charset=UTF-8",
-            },
-        });
-    } else if (url.pathname === "/download") {
-        console.log('Starting Download page');
-        response = new Response(Bun.file("./src/HTML/download.html"), {
-            headers: {
-                "Content-Type": "text/html, charset=UTF-8",
-            },
-        });
-    } else if (url.pathname === "/discord") {
-        console.log('Discord Redirecting');
-        response = Response.redirect("https://discord.gg/teaclient");
-    } else if (url.pathname.startsWith("/CSS/")) {
-        response = new Response(Bun.file("./Src" + url.pathname), {
-            headers: {
-                "Content-Type": "text/css",
-            },
-        });
+      response = new Response(Bun.file("./src/HTML/index.html"));
     }
-    
+
+    if (url.pathname === "/download") {
+      console.log("Starting Download page");
+      response = new Response(Bun.file("./src/HTML/download.html"));
+    }
+
+    if (url.pathname === "/discord") {
+      console.log("Discord Redirecting");
+      response = Response.redirect("https://discord.gg/teaclient");
+    }
+
+    if (url.pathname.startsWith("/Assets/")) {
+      response = new Response(Bun.file("./Src" + url.pathname), {});
+    }
+
+    if (url.pathname.startsWith("/CSS/")) {
+      response = new Response(Bun.file("./Src" + url.pathname), {});
+    }
+
+
     return response;
-    
-
-
-
-
-
   },
 });
-
-
-
-
-
-function htmlSuupport() {
-    if (url.pathname === "/") {
-        console.log('Starting Home page');
-        response = new Response(Bun.file("./Src/HTML/index.html"), {
-            headers: {
-                "Content-Type": "text/html, charset=UTF-8",
-            },
-        });
- }
-
-
-
-
- return response;
-}

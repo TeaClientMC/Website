8068134b8546052e87fd3c1be16173e7cb759d0b
eveeify <eveeg1971@gmail.com>
2023-10-09 19:15:28 +1100

    Removed: testing. Fixed: orgnization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vue from "@astrojs/vue";
-import react from "@astrojs/react";
-import solidJs from "@astrojs/solid-js";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +8,5 @@ export default defineConfig({
     port: 7053
   },
   cacheDir: "./src/*",
-  integrations: [mdx(), react(), vue(), solidJs(), svelte()]
+  integrations: [mdx()]
 });

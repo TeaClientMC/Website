05f9064465c0eaeebfbea6814be1fe0a4c57b505
eveeify <eveeg1971@gmail.com>
2023-11-08 23:10:34 +1100

    Feat: Improved responsiveness Feat: Screenshot Now Working Feat: Lint now Working.

--- screenshot.ts
+++ screenshot.ts
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 
-export async function takeScreenshot() {
+
     const browser = await puppeteer.launch({
         headless: "new",
         ignoreHTTPSErrors: true
@@ -8,7 +8,7 @@ export async function takeScreenshot() {
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080}); // Set viewport size
     await page.goto('http://localhost:7053');
-    await page.screenshot({ path: './public/screenshot.webp', type: 'webp' });
+    await page.screenshot({ path: 'public/screenshot.webp', type: 'webp' });
     await browser.close();
-    return 'Screenshot taken!';
-}
+    console.log('Screenshot taken!');
+    process.exit(1)

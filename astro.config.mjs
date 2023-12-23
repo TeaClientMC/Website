47b59986f22d71022cb5ab241ae0c27d2fbf274c
eveeify <eveeg1971@gmail.com>
2023-12-23 22:47:04 +1100

    chore: news page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://teaclient.net',
   server: {
-    port: 7053,
+    port: 7053
   },
   integrations: [mdx(), tailwind()],
-  experimental: {},
+  experimental: {}
 });

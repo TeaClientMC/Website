56783eba11c59a7964299ec8eed4d28d4661dc4d
eveeify <eveeg1971@gmail.com>
2024-01-25 19:55:07 +1100

    feat: socials

--- astro.config.ts
+++ astro.config.ts
@@ -3,29 +3,31 @@ import mdx from '@astrojs/mdx';
 import postcssImport from 'postcss-import';
 import tailwindcssNesting from 'tailwindcss/nesting';
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer'
+import autoprefixer from 'autoprefixer';
 import tailwind from '@astrojs/tailwind';
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teaclient.net',
   server: {
-    port: 7053,
+    port: 7053
   },
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), metaTags()],
   experimental: {},
   redirects: {
-    '/discord': 'https://discord.gg/ejFTe4Hfnc',
+    '/discord': 'https://discord.gg/ejFTe4Hfnc'
   },
   vite: {
+    resolve: {
+      alias: {
+        '@assets': '/src/assets/'
+      }
+    },
     css: {
       postcss: {
-        plugins: [
-          postcssImport(),
-          tailwindcssNesting(),
-          tailwindcss(),
-          autoprefixer()
-        ]
+        plugins: [postcssImport(), tailwindcssNesting(), tailwindcss(), autoprefixer()]
       }
     }
   }

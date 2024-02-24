10b9816e16b72e858b94377cd5fe0ee33f6cb2d9
eveeify <eveeg1971@gmail.com>
2024-02-24 22:54:36 +1100

    chore: added changes from my improments ui branch

---
+++ astro.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import postcssImport from 'postcss-import';
+import tailwindcssNesting from 'tailwindcss/nesting';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwind from '@astrojs/tailwind';
+
+import metaTags from "astro-meta-tags";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://teaclient.net',
+  server: {
+    port: 7053
+  },
+  integrations: [mdx(), tailwind(), metaTags()],
+  experimental: {},
+  redirects: {
+    '/discord': 'https://discord.gg/ejFTe4Hfnc',
+    '/klaas': 'https://klaas.tiiny.site/'
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': '/src/assets/'
+      }
+    },
+    css: {
+      postcss: {
+        plugins: [postcssImport(), tailwindcssNesting(), tailwindcss(), autoprefixer()]
+      }
+    }
+  }
+});

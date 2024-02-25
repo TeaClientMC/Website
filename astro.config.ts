fcaa9c7849314644846747fc88b9a5777f41b3cc
eveeify <eveeg1971@gmail.com>
2024-02-25 11:00:01 +1100

    feat: github link

--- astro.config.ts
+++ astro.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   redirects: {
     '/discord': 'https://discord.gg/ejFTe4Hfnc',
     '/klaas': 'https://klaas.tiiny.site/',
+    '/github': 'https://github.com/TeaClientMC'
   },
   vite: {
     resolve: {

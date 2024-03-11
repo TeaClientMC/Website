d312cb099bd110aa5a254422fdb9b05d22e5d230
eveeify <eveeg1971@gmail.com>
2024-03-11 20:48:56 +1100

    feat: title prop, banner, better props

--- astro.config.ts
+++ astro.config.ts
@@ -1,35 +1,30 @@
-import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import metaTags from "astro-meta-tags";
+import { defineConfig } from "astro/config";
 
-import metaTags from 'astro-meta-tags';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://teaclient.net',
-  server: {
-    port: 7053,
-  },
-  integrations: [mdx(), metaTags()],
-  experimental: {},
-  redirects: {
-    '/discord': 'https://discord.gg/ejFTe4Hfnc',
-    '/klaas': 'https://klaas.tiiny.site/',
-    '/github': 'https://github.com/TeaClientMC'
-  },
-  vite: {
-    resolve: {
-      alias: {
-        '@assets': '/src/assets/',
-      },
-    },
-    build: {
-      minify: true,
-      sourcemap: true,
-    },
-    plugins: [tailwindcss()],
-    css: {
-      transformer: 'lightningcss'
-    }
-  },
+	site: "https://teaclient.net",
+	server: {
+		port: 7053,
+	},
+	integrations: [mdx(), metaTags(), tailwind({ nesting: true })],
+	redirects: {
+		"/discord": "https://discord.gg/ejFTe4Hfnc",
+		"/klaas": "https://klaas.tiiny.site/",
+		"/github": "https://github.com/TeaClientMC",
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@assets": "/src/assets/",
+			},
+		},
+		build: {
+			minify: true,
+			sourcemap: true,
+		},
+	},
 });

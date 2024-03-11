4791beccb8ed416554167952ad1f3e34cf1b8378
eveeify <eveeg1971@gmail.com>
2024-03-12 00:57:10 +1100

    feat: styling rss feed

--- src/pages/news/index.xml.ts
+++ src/pages/news/index.xml.ts
@@ -1,4 +1,4 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems} from "@astrojs/rss";
 
 export async function GET(context) {
 	return rss({
@@ -7,5 +7,7 @@ export async function GET(context) {
 		site: context.site,
 		items: await pagesGlobToRssItems(import.meta.glob("./*.{md,mdx}")),
 		customData: "<language>en-us</language>",
+		trailingSlash: false,
+		stylesheet: './feed.xsl'
 	});
 }

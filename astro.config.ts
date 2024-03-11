1516c1de6b674f97e7b12fef9fb483a45ac35891
eveeify <eveeg1971@gmail.com>
2024-03-11 23:10:39 +1100

    chore: removed the need of manual sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,34 +1,37 @@
 import mdx from "@astrojs/mdx";
 import metaTags from "astro-meta-tags";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://teaclient.net",
-	server: {
-		port: 7053,
-	},
-	integrations: [mdx(), metaTags(), tailwind({ nesting: true })],
-	redirects: {
-		"/discord": "https://discord.gg/ejFTe4Hfnc",
-		"/klaas": "https://klaas.tiiny.site/",
-		"/github": "https://github.com/TeaClientMC",
-	},
-	vite: {
-		resolve: {
-			alias: {
-				'@layout': 'src/layouts',
-				'@component': 'src/components',
-				'@scripts': 'src/scripts',
-				'@styles': 'src/styles',
-				'@assets': 'src/assets',
-			},
-		},
-		build: {
-			minify: true,
-			sourcemap: true,
-		},
-	},
+  site: "https://teaclient.net",
+  server: {
+    port: 7053
+  },
+  integrations: [mdx(), metaTags(), tailwind({
+    nesting: true
+  }), sitemap()],
+  redirects: {
+    "/discord": "https://discord.gg/ejFTe4Hfnc",
+    "/klaas": "https://klaas.tiiny.site/",
+    "/github": "https://github.com/TeaClientMC"
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@layout': 'src/layouts',
+        '@component': 'src/components',
+        '@scripts': 'src/scripts',
+        '@styles': 'src/styles',
+        '@assets': 'src/assets'
+      }
+    },
+    build: {
+      minify: true,
+      sourcemap: true
+    }
+  }
 });

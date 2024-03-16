ee459d48f4efe679c3c1bfd0b3f207c92c567f05
eveeify <eveeg1971@gmail.com>
2024-03-16 21:12:34 +1100

    fix: formating and linting

--- astro.config.ts
+++ astro.config.ts
@@ -1,36 +1,41 @@
 import mdx from "@astrojs/mdx";
-import metaTags from "astro-meta-tags";
-import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import metaTags from "astro-meta-tags";
 import tailwindV4 from "astro-tailwind-v4";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://teaclient.net",
-  server: {
-    port: 7053
-  },
-  integrations: [mdx(), metaTags(), sitemap({
-    customPages: ["https://teaclient.net/docs"]
-  }), tailwindV4({applyBaseStyles: false})],
-  redirects: {
-    "/discord": "https://discord.gg/ejFTe4Hfnc",
-    "/klaas": "https://klaas.tiiny.site/",
-    "/github": "https://github.com/TeaClientMC"
-  },
-  vite: {
-    resolve: {
-      alias: {
-        "@layout": "src/layouts",
-        "@component": "src/components",
-        "@scripts": "src/scripts",
-        "@styles": "src/styles",
-        "@assets": "src/assets"
-      }
-    },
-    build: {
-      minify: true,
-      sourcemap: false
-    }
-  }
+	site: "https://teaclient.net",
+	server: {
+		port: 7053,
+	},
+	integrations: [
+		mdx(),
+		metaTags(),
+		sitemap({
+			customPages: ["https://teaclient.net/docs"],
+		}),
+		tailwindV4({ applyBaseStyles: false }),
+	],
+	redirects: {
+		"/discord": "https://discord.gg/ejFTe4Hfnc",
+		"/klaas": "https://klaas.tiiny.site/",
+		"/github": "https://github.com/TeaClientMC",
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@layout": "src/layouts",
+				"@component": "src/components",
+				"@scripts": "src/scripts",
+				"@styles": "src/styles",
+				"@assets": "src/assets",
+			},
+		},
+		build: {
+			minify: true,
+			sourcemap: false,
+		},
+	},
 });

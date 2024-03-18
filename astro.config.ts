924ea650ebc354bd85a1291d3e755be5a4b6df34
eveeify <eveeg1971@gmail.com>
2024-03-19 04:44:46 +1100

    fix: formating and linting

--- astro.config.ts
+++ astro.config.ts
@@ -7,29 +7,34 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://teaclient.net",
-  server: {
-    port: 7053
-  },
-  integrations: [mdx(), metaTags(), sitemap(), tailwind({nesting: true, applyBaseStyles: true})],
-  redirects: {
-    "/discord": "https://discord.gg/ejFTe4Hfnc",
-    "/klaas": "https://klaas.tiiny.site/",
-    "/github": "https://github.com/TeaClientMC"
-  },
-  vite: {
-    resolve: {
-      alias: {
-        "@layout": "src/layouts",
-        "@component": "src/components",
-        "@scripts": "src/scripts",
-        "@styles": "src/styles",
-        "@assets": "src/assets"
-      }
-    },
-    build: {
-      minify: true,
-      sourcemap: false
-    }
-  }
+	site: "https://teaclient.net",
+	server: {
+		port: 7053,
+	},
+	integrations: [
+		mdx(),
+		metaTags(),
+		sitemap(),
+		tailwind({ nesting: true, applyBaseStyles: true }),
+	],
+	redirects: {
+		"/discord": "https://discord.gg/ejFTe4Hfnc",
+		"/klaas": "https://klaas.tiiny.site/",
+		"/github": "https://github.com/TeaClientMC",
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@layout": "src/layouts",
+				"@component": "src/components",
+				"@scripts": "src/scripts",
+				"@styles": "src/styles",
+				"@assets": "src/assets",
+			},
+		},
+		build: {
+			minify: true,
+			sourcemap: false,
+		},
+	},
 });

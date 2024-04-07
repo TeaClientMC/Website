05f23a6887bbbb30eb551637ca33841d87c2974b
eveeifyeve <eveeg1971@gmail.com>
2024-04-08 01:46:41 +1000

    fix: quality checks and flake

--- flake.nix
+++ flake.nix
@@ -10,70 +10,73 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            nodejs-packages = with pkgs.nodePackages; [
-              vscode-langservers-extracted
-              typescript-language-server
-              yarn
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nodejs-packages = with pkgs.nodePackages; [
+            vscode-langservers-extracted
+            typescript-language-server
+          ];
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                difftastic.enable = true;
+
+                # Enviroment Virables
+                env = { };
+
+                # https://devenv.sh/reference/options/
+                packages = with pkgs; [ ] ++ nodejs-packages;
+
+                # https://devenv.sh/scripts/
+                # scripts.hello.exec = "";
+
+                # enterShell = ''
+                # '';
+
+                # https://devenv.sh/languages/
+                languages.javascript = {
+                  enable = true;
+                  bun = {
+                    enable = true;
+                    install.enable = true;
+                  };
+                };
+
+                languages.typescript = {
+                  enable = true;
+                };
+
+                # https://devenv.sh/pre-commit-hooks/
+                pre-commit.hooks = {
+                  yamllint.settings.preset = "relaxed";
+                  nixfmt.package = pkgs.nixfmt-rfc-style;
+                  nixfmt.enable = true;
+                  rome.enable = true;
+                };
+
+                # https://devenv.sh/integrations/dotenv/
+                dotenv.enable = true;
+              }
             ];
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  difftastic.enable = true;
-
-                  # Enviroment Virables
-                  env = {
-                  };
-
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs;
-                    [] ++ nodejs-packages;
-
-                  # https://devenv.sh/scripts/
-                  # scripts.hello.exec = "";
-
-                  # enterShell = ''
-                  # '';
-
-                  # https://devenv.sh/languages/
-                  languages.javascript = {
-                    enable = true;
-                    bun = {
-                      enable = true;
-                      install.enable = true;
-                    };
-                  };
-
-                  languages.typescript = {
-                    enable = true;
-                  };
-
-                  # https://devenv.sh/pre-commit-hooks/
-                  pre-commit.hooks = {
-                    nixfmt.package = pkgs.nixfmt-rfc-style;
-                    nixfmt.enable = true;
-                    yamllint.enable = true;
-                    editorconfig-checker.enable = true;
-                    rome.enable = true;
-                  };
-
-                  # https://devenv.sh/integrations/dotenv/
-                  dotenv.enable = true;
-
-                }
-              ];
-            };
-          });
+          };
+        }
+      );
     };
 }

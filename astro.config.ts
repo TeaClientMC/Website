c8547b3b5935fb4e1e9802f157c14a67d203deda
eveeifyeve <eveeg1971@gmail.com>
2024-04-08 00:55:54 +1000

    fix: formatted and linted

--- astro.config.ts
+++ astro.config.ts
@@ -7,41 +7,41 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://teaclient.net",
-  server: {
-    port: 7053,
-  },
-  integrations: [
-    mdx(),
-    metaTags(),
-    sitemap({ exclude: ["/roblox"] }),
-    tailwind({ nesting: true, applyBaseStyles: true }),
-  ],
-  redirects: {
-    // Resources
-    "/discord": "https://discord.gg/ejFTe4Hfnc",
-    "/github": "https://github.com/TeaClientMC",
-    "/roadmap": "https://github.com/orgs/TeaClientMC/projects/3",
+	site: "https://teaclient.net",
+	server: {
+		port: 7053,
+	},
+	integrations: [
+		mdx(),
+		metaTags(),
+		sitemap({ exclude: ["/roblox"] }),
+		tailwind({ nesting: true, applyBaseStyles: true }),
+	],
+	redirects: {
+		// Resources
+		"/discord": "https://discord.gg/ejFTe4Hfnc",
+		"/github": "https://github.com/TeaClientMC",
+		"/roadmap": "https://github.com/orgs/TeaClientMC/projects/3",
 
-    // Partner Networks
-    "/dropps": "https://discord.gg/qFarXHT32J",
+		// Partner Networks
+		"/dropps": "https://discord.gg/qFarXHT32J",
 
-    // Easter-Eggs
-    "/klaas": "https://klaas.tiiny.site/",
-  },
-  vite: {
-    resolve: {
-      alias: {
-        "@layout": "src/layouts",
-        "@component": "src/components",
-        "@scripts": "src/scripts",
-        "@styles": "src/styles",
-        "@assets": "src/assets",
-      },
-    },
-    build: {
-      minify: true,
-      sourcemap: false,
-    },
-  },
+		// Easter-Eggs
+		"/klaas": "https://klaas.tiiny.site/",
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@layout": "src/layouts",
+				"@component": "src/components",
+				"@scripts": "src/scripts",
+				"@styles": "src/styles",
+				"@assets": "src/assets",
+			},
+		},
+		build: {
+			minify: true,
+			sourcemap: false,
+		},
+	},
 });

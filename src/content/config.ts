1d982f9228d5228e7538c1f76b935cb1958abca6
eveeifyeve <eveeg1971@gmail.com>
2024-07-07 17:05:42 +1000

    fix: biome formating

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const news = defineCollection({
-	type: 'content',
+	type: "content",
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		heroImage: z.string().optional(),
-        authorBlog: z.object({
-            name: z.string(),
-            role: z.string(),
-            socials: z.object({
-                github: z.string().optional(),
-            })
-        })
+		authorBlog: z.object({
+			name: z.string(),
+			role: z.string(),
+			socials: z.object({
+				github: z.string().optional(),
+			}),
+		}),
 	}),
 });
 
-
 const wiki = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-    }),
-})
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+	}),
+});
 
 export const collections = { news, wiki };
--- src/pages/news/index.xml.ts
+++ src/pages/news/index.xml.ts
@@ -1,9 +1,9 @@
+import { getCollection } from "astro:content";
 import rss, {
 	pagesGlobToRssItems,
 	type RSSFeedItem,
 	type RSSOptions,
 } from "@astrojs/rss";
-import { getCollection } from "astro:content";
 
 export async function GET(context: RSSOptions) {
 	const posts = await getCollection("news");
@@ -11,10 +11,10 @@ export async function GET(context: RSSOptions) {
 		title: "TeaClient News/Updates",
 		description: "Update your self on the latest news.",
 		site: context.site,
-		items:  posts.map((post) => ({
+		items: posts.map((post) => ({
 			...post.data,
 			link: `/news/${post.slug}/`,
-		}))
+		})),
 		customData: "<language>en-us</language>",
 		trailingSlash: false,
 	});

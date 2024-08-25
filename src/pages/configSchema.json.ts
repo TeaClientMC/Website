044ddbb7082172ad5a9b5f7ee177245ef1002181
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-25 22:52:33 +1000

    fix: config schema

--- src/pages/configSchema.json.ts
+++ src/pages/configSchema.json.ts
@@ -6,31 +6,40 @@ export const GET = async () => {
 	});
 
 	try {
-		let data = await octokit.request(
-			"GET /repos/{owner}/{repo}/releases/latest",
-			{
-				owner: "TeaClientMC",
-				repo: "ConfigSchema",
-				headers: {
-					"X-GitHub-Api-Version": "2022-11-28",
-				},
-			},
-		);
-
-		// Access the assets array from the response
+		const owner = "TeaClientMC";
+		const repo = "json-schemas";
+		let data = await octokit.rest.repos.getLatestRelease({ owner, repo });
 		const assets = data.data.assets;
+		const schema = assets.find((asset) => asset.name === "configSchema.json");
 
-		// Find the JSON file and get its download URL
-		const jsonFileUrl = assets.find(
-			(asset) => asset.name === "configschema.json",
-		).browser_download_url;
+		// Check if the config schema is found
+		if (!schema) {
+			return Error("Config schema not found");
+		}
 
-		// Download the JSON file content
-		const response = await fetch(jsonFileUrl);
-		const jsonContent = await response.text();
+		const {url} = await octokit.request(
+			'GET /repos/{owner}/{repo}/releases/assets/{asset_id}',
+			{
+				owner,
+				repo,
+				headers: {
+					accept: "application/octet-stream",
+				},
+				asset_id: schema.id,
+			},
+		)
 
-		// Return the JSON content
-		return new Response(jsonContent);
+		const response = await fetch(url, { 
+			method: "GET",
+		})
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+
+		const responseText = await response.json()
+
+		return new Response(JSON.stringify(responseText), { status: 200 });
 	} catch (error) {
 		console.error("Error fetching config schema:", error);
 		return new Response(

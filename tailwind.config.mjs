11234c0b32809b2edd44702aba961946a2f2bc78
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-09-05 23:03:10 +1000

    chore: moved tab component to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        tabgroup_transisiton: "transition: background-color 0.3s",
+      },
+    },
+  },
+  plugins: [],
 };
